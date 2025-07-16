ae3084b331875105eba44151c60feae076ac1926
Dog-Fan1 <henrydeller11934@gmail.com>
2025-07-16 20:58:10 +1000

    Update server.js to correct port

--- server.js
+++ server.js
@@ -57,7 +57,7 @@ app.post('/wipe', (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
